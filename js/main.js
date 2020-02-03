d9de38bd8b6f79d2f485741997e25984923839e9
Stheven Cabral <stheven.cabral@gmail.com>
2020-02-03 13:56:10 -0800

    02/03/2020  1355
    - Start search function.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+/* Options for the lightbox plugin*/
 $('.gallery a').lightbox({
     margin: 200,
-    
-
 });
+
+/*Code for the search bar*/
+$(document).ready(function(){
+    $("search").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#lightgallery a").filter(function() {
+        $(this).toggle($(this).attr("data-caption").toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
