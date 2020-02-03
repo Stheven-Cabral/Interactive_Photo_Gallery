d83823dc3e03f1ef66d70b7152aaf10661b2aa16
Stheven Cabral <stheven.cabral@gmail.com>
2020-02-03 15:22:03 -0800

    02/03/2020 1521

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 /* Options for the lightbox plugin*/
 $('.gallery a').lightbox({
-    margin: 200,
+    margin: 200
 });
 
 /*Code for the search bar*/
 $(document).ready(function(){
-    $("search").on("keyup", function() {
+    $("#search").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $("#lightgallery a").filter(function() {
         $(this).toggle($(this).attr("data-caption").toLowerCase().indexOf(value) > -1)
