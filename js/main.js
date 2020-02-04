d134d07c6294d8e95fad623f0edf3f8c5f67955d
Stheven Cabral <stheven.cabral@gmail.com>
2020-02-03 19:13:57 -0800

    02/03/2020  1913
    - Commit before using a filter plugin.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-/* Options for the lightbox plugin*/
-$('.gallery a').lightbox({
-    margin: 200
-});
-
 /*Code for the search bar*/
 $(document).ready(function(){
     $("#search").on("keyup", function() {
       var value = $(this).val().toLowerCase();
-      $("#lightgallery a").filter(function() {
+
+      $(".gallery a").filter(function() {
         $(this).toggle($(this).attr("data-caption").toLowerCase().indexOf(value) > -1)
       });
     });
   });
+
+/* Options for the lightbox plugin*/
+$('.gallery a').lightbox({
+  margin: 200
+});
+
