bd7cac73c5a84bfc7ca463d6507642f787ebfe42
Stheven Cabral <stheven.cabral@gmail.com>
2020-02-03 22:24:33 -0800

    02/03/2020 Final

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /*Code for the search bar*/
 $(document).ready(function(){
-    $("#search").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
+  $("#search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
 
-      $(".gallery a").filter(function() {
-        $(this).toggle($(this).attr("data-caption").toLowerCase().indexOf(value) > -1)
-      });
+    $(".gallery a").filter(function() {
+      $(this).toggle($(this).attr("data-caption").toLowerCase().indexOf(value) > -1);
     });
   });
+});
 
 /* Options for the lightbox plugin*/
 $('.gallery a').lightbox({
